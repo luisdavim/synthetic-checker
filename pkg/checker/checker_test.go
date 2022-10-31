ac8655311383018c2c0cd15f2ae7a45122057dec
Luis Davim <luis.davim@gmail.com>
2022-10-31 22:58:43 +0000

    test: add tests for Sync

--- pkg/checker/checker_test.go
+++ pkg/checker/checker_test.go
@@ -194,3 +194,164 @@ func TestEvaluate(t *testing.T) {
 		})
 	}
 }
+
+func TestSync(t *testing.T) {
+	tests := []struct {
+		name     string
+		config   config.Config
+		status   string
+		expected api.Status
+	}{
+		{
+			name: "Http OK",
+			config: config.Config{
+				HTTPChecks: map[string]config.HTTPCheck{
+					checkName: {
+						URL:    "http://fake.com/ok",
+						Method: http.MethodGet,
+					},
+				},
+			},
+			status: `{
+    "test-http": {
+        "ok": true,
+        "timestamp": "2022-10-31T22:43:56.715843212Z",
+        "duration": 918525104,
+        "contiguousFailures": 0,
+        "timeOfFirstFailure": "0001-01-01T00:00:00Z"
+    }
+}`,
+			expected: api.Status{
+				OK:    true,
+				Error: "",
+			},
+		},
+		{
+			name: "Http Not OK",
+			config: config.Config{
+				HTTPChecks: map[string]config.HTTPCheck{
+					checkName: {
+						URL:    "http://fake.com/ok",
+						Method: http.MethodGet,
+					},
+				},
+			},
+			status: `{
+    "test-http": {
+        "error": "Unexpected status code: '500' expected: '200'",
+        "timestamp": "2022-10-31T22:43:56.715808368Z",
+        "duration": 918794374,
+        "contiguousFailures": 1,
+        "timeOfFirstFailure": "2022-10-31T22:43:56.715808368Z"
+    }
+}`,
+			expected: api.Status{
+				OK:                 false,
+				Error:              "Unexpected status code: '500' expected: '200'",
+				ContiguousFailures: 1,
+			},
+		},
+		{
+			name: "DNS OK",
+			config: config.Config{
+				DNSChecks: map[string]config.DNSCheck{
+					checkName: {
+						Host: "www.google.com",
+					},
+				},
+			},
+			status: `{
+    "test-dns": {
+        "ok": true,
+        "timestamp": "2022-10-31T22:43:56.715846962Z",
+        "duration": 27122865,
+        "contiguousFailures": 0,
+        "timeOfFirstFailure": "0001-01-01T00:00:00Z"
+    }
+}`,
+			expected: api.Status{
+				OK: true,
+			},
+		},
+		{
+			name: "multiple OK",
+			config: config.Config{
+				DNSChecks: map[string]config.DNSCheck{
+					checkName: {
+						Host: "www.google.com",
+					},
+				},
+				HTTPChecks: map[string]config.HTTPCheck{
+					checkName: {
+						URL:    "http://fake.com/ok",
+						Method: http.MethodGet,
+					},
+				},
+				ConnChecks: map[string]config.ConnCheck{
+					checkName: {
+						Address: "www.google.com:443",
+					},
+				},
+			},
+			status: `{
+    "test-conn": {
+        "ok": true,
+        "timestamp": "2022-10-31T22:43:56.715798368Z",
+        "duration": 267292,
+        "contiguousFailures": 0,
+        "timeOfFirstFailure": "0001-01-01T00:00:00Z"
+    },
+    "test-dns": {
+        "ok": true,
+        "timestamp": "2022-10-31T22:43:56.715846962Z",
+        "duration": 27122865,
+        "contiguousFailures": 0,
+        "timeOfFirstFailure": "0001-01-01T00:00:00Z"
+    },
+    "test-http": {
+        "ok": true,
+        "timestamp": "2022-10-31T22:43:56.715843212Z",
+        "duration": 918525104,
+        "contiguousFailures": 0,
+        "timeOfFirstFailure": "0001-01-01T00:00:00Z"
+    }
+}`,
+			expected: api.Status{
+				OK: true,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			httpmock.Activate()
+			defer httpmock.DeactivateAndReset()
+			httpmock.RegisterResponder(http.MethodGet, "http://leader:8080/", httpmock.NewStringResponder(http.StatusOK, tt.status))
+			c, err := NewFromConfig(tt.config)
+			defer func() {
+				// avoid panic with the prometheus.MustRegister used in NewFromConfig
+				prometheus.Unregister(checkStatus)
+				prometheus.Unregister(checkDuration)
+			}()
+			if err != nil {
+				t.Errorf("unexpected error: %v", err)
+			}
+			c.Sync("leader")
+			for name := range c.checks {
+				actual, ok := c.GetStatusFor(name)
+				if !ok {
+					t.Errorf("missing status for %s", name)
+				}
+				if actual.OK != tt.expected.OK {
+					t.Errorf("unexpected status, wanted: %t, got: %t", tt.expected.OK, actual.OK)
+				}
+				if actual.Error != tt.expected.Error {
+					t.Errorf("unexpected error, wanted: %s, got: %s", tt.expected.Error, actual.Error)
+				}
+				if actual.ContiguousFailures != tt.expected.ContiguousFailures {
+					t.Errorf("unexpected number of contiguous failures, wanted: %d, got: %d", tt.expected.ContiguousFailures, actual.ContiguousFailures)
+				}
+			}
+		})
+	}
+}
